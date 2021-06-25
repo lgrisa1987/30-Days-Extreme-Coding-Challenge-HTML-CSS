70340318ff26ca3f0ebc720babf1df79811a362d
Leandro <leagrisalena@gmail.com>
2021-06-25 00:48:04 -0300

    02-Neumorphism-Login-Form - js part-3

---
+++ 02-Neumorphism-Login-Form/script.js
@@ -0,0 +1,5 @@
+const switchCtn = document.querySelector('#switch-ctn'),
+    switchC1 = document.querySelector('#switch-c1'),
+    switchC2 = document.querySelector('#switch-c2'),
+    switchCircle = [].slice.call(document.querySelectorAll('.switch__circle')),
+    switchBtn = [].slice.call(document.querySelectorAll('.switch-btn'))
