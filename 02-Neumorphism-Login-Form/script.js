428cf9b69992e9418d19f6c04b973abfcb8e2208
Leandro <leagrisalena@gmail.com>
2021-06-27 02:58:53 -0300

    02-Neumorphism-Login-Form - final

--- 02-Neumorphism-Login-Form/script.js
+++ 02-Neumorphism-Login-Form/script.js
@@ -1,5 +1,51 @@
-const switchCtn = document.querySelector('#switch-ctn'),
-    switchC1 = document.querySelector('#switch-c1'),
-    switchC2 = document.querySelector('#switch-c2'),
-    switchCircle = [].slice.call(document.querySelectorAll('.switch__circle')),
-    switchBtn = [].slice.call(document.querySelectorAll('.switch-btn'))
+const select = selector => document.querySelector(selector),
+    selectAll = selector => [].slice.call(document.querySelectorAll(selector)),
+    switchCtn = select('#switch-ctn'),
+    switchC1 = select('#switch-c1'),
+    switchC2 = select('#switch-c2'),
+    switchCircle = selectAll('.switch__circle'),
+    switchBtn = selectAll('.switch-btn'),
+    aContainer = select('#a-container'),
+    bContainer = select('#b-container'),
+    allButtons = selectAll('.submit'),
+    getButtons = e => e.preventDefault(),
+    toggleClass = (el = el, clase) => el.classList.toggle(clase),
+    changeForm = () => {
+        switchCtn.classList.add('is-gx');
+        setTimeout(() => {
+            switchCtn.classList.remove('is-gx');
+        }, 1500);
+        [switchCtn, ...switchCircle, switchC1, switchC2, aContainer, bContainer].forEach((el, index) => {
+            switch (index) {
+                case 0:
+                case 1:
+                case 2:
+                    toggleClass(el, 'is-txr');
+                    break;
+                case 3:
+                case 4:
+                    toggleClass(el, 'is-hidden');
+                    break;
+                case 5:
+                    toggleClass(el, 'is-txl');
+                    break;
+                default:
+                    ['is-txl', 'is-z200'].forEach(clase => toggleClass(el, clase));
+                    break;
+            }
+        });
+    }
+
+allButtons.forEach((element, index) => {
+    element.addEventListener('click', getButtons)
+    switchBtn[index].addEventListener('click', changeForm);
+});
+
+WebFont.load({
+    google: {
+        families: ['Montserrat']
+    },
+    active() {
+        select('.main').classList.add('fadeIn');
+    }
+});
